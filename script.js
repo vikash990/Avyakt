1748e3ac3acc29adba6b92e50535579311c358ce
aman dubey <dubey.amansus@gmail.com>
2020-08-18 11:48:58 -0700

    avyakt

---
+++ script.js
@@ -0,0 +1,18 @@
+var car={
+    model:500,
+    name:"Swift",
+    cost:9000
+}
+
+
+console.log(car["name"])
+
+
+function displayDate(){
+    document.getElementById("demo").innerHTML=Date()
+}
+
+function changeText(id){
+    id.innerHTML="Bye!"
+
+}
